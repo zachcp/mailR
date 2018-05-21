c2ef8ef9a5d4c20a727603f44191f5efee6cfe4e
zach charlop-powers <zach.charlop.powers@gmail.com>
2018-05-21 17:38:04 -0400

    update jars and add javax

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  .jpackage(pkgname)
+  rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
+  rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
 }
